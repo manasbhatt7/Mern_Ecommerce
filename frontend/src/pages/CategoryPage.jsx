06c8c377853eea1e9515ac3685f30820e47f5230
manasbhatt7 <manasbhatt007@gmail.com>
2025-03-08 17:53:36 +0530

    categoty page up

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -14,6 +14,7 @@ const CategoryPage = () => {
 	}, [fetchProductsByCategory, category]);
 
 	console.log("products:", products);
+
 	return (
 		<div className='min-h-screen'>
 			<div className='relative z-10 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
